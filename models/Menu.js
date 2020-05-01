62746491d1c5561605f8e6acafdfc801803a0f15
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-01 15:36:18 +0700

    done

--- models/Menu.js
+++ models/Menu.js
@@ -1,15 +1,22 @@
 const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("cafe", "root", "Hallo123$", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const env = process.env.NODE_ENV || "development";
+const config = (require = require(__dirname + "/../config/config.json")[[env]]);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+// const sequelize = new Sequelize("cafe", "root", "Hallo123$", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// });
 
 const Menu = sequelize.define(
   "menu",
